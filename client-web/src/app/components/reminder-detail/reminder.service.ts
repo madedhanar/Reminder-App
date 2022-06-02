4c9e5d47f2c95a49a16dca8f2484076573c85eaf
madedhanar <50219598+madedhanar@users.noreply.github.com>
2022-06-02 18:43:45 -0400

    get patient list

--- client-web/src/app/components/reminder-detail/reminder.service.ts
+++ client-web/src/app/components/reminder-detail/reminder.service.ts
@@ -10,11 +10,17 @@ import { Reminder } from 'src/app/reminder';
     )
 export class ReminderService {
   private baseURL = "http://localhost:8080/api/v1/reminder";
+  private getPatientURL = "http://localhost:8080/api/v1/patient/doctor/1"
   constructor(private http: HttpClient) { }
   
     //get API Reminder
     getReminderList(): Observable<Reminder[]>{
         return this.http.get<Reminder[]>(`${this.baseURL}`);
+    }
+
+    //get PatientList API Reminder
+    getPatientList(): Observable<Reminder[]>{
+        return this.http.get<Reminder[]>(`${this.getPatientURL}`);
     }
     
     //post API Reminder
