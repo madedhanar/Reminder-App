745419a72fbc3350c076c87268d110d702619a36
madedhanar <50219598+madedhanar@users.noreply.github.com>
2022-06-02 00:01:03 -0400

    Add find reminder by doctor and patient id

---
+++ reminderBackend/src/test/java/com/example/reminderBackend/ReminderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reminderBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
